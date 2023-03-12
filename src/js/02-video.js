6906d4ce8d473dd7a523631d2cea4562a24118fe
DemoUser77 <katykatykaty001@gmail.com>
2023-03-13 00:00:28 +0200

    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const currentTime = Number(localStorage.getItem(LOCALSTORAGE_KEY));
+
+const onPlay = function(data) {
+    localStorage.getItem(LOCALSTORAGE_KEY, data.seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+import throttle from 'lodash.throttle';
 
+form = document.querySelector('.feedback-form');
+LOCAL_KEY = "feedback-form-state";
+
+form.addEventListener('input',);
+
+player.on('timeupdate', throttle(onPlay, 500));
