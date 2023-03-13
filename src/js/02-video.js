c565d14ffb0cda2d6bda164b34f9fb4fce170466
DemoUser77 <katykatykaty001@gmail.com>
2023-03-13 15:45:18 +0200

    change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,10 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
-const currentTime = Number(localStorage.getItem(LOCALSTORAGE_KEY));
+
 
 const onPlay = function(data) {
-    localStorage.getItem(LOCALSTORAGE_KEY, data.seconds)
+    localStorage.setItem("videoplayer-current-time", data.seconds)
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
@@ -16,16 +15,4 @@ player.getVideoTitle().then(function(title) {
         console.log('title:', title);
     });
 
-player.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(localStorage.setItem("videoplayer-current-time"));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,44 @@
 import throttle from 'lodash.throttle';
 
-form = document.querySelector('.feedback-form');
-LOCAL_KEY = "feedback-form-state";
+const formEl = document.querySelector('.feedback-form');
+const textareaEl = document.querySelector('.feedback-form textarea')
 
-form.addEventListener('input',);
+const LOCAL_KEY = "feedback-form-state";
+const formData = {};
 
-player.on('timeupdate', throttle(onPlay, 500));
+populetTextarea();
+
+formEl.addEventListener('submit', handleFormSubmit);
+textareaEl.addEventListener('input', throttle(handleTextareaInput, 500));
+
+
+function handleTextareaInput(event) {
+    const message = event.target.value;
+
+    localStorage.setItem(LOCAL_KEY, message);
+
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCAL_KEY);
+}
+
+function populetTextarea() {
+    const savedMessage = localStorage.getItem(LOCAL_KEY);
+
+    if (savedMessage) {
+        console.log(savedMessage);
+        textareaEl.value = savedMessage;
+    }
+    
+}
+
+formEl.addEventListener('input', e => {
+    formData[e.target.name] = e.target.value;
+
+    console.log(formData);
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+})
